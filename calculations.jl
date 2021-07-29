6a8932e28e2bf2cbd51cfef770c9a6f8279b5e5b
Diana Khimey <dianak@blc00.bl.pvt>
2021-07-29 16:54:09 +0000

    adding @view to improve performance

--- calculations.jl
+++ calculations.jl
@@ -26,19 +26,20 @@ module HashpipeCalculations
         end
     end
 
-    function compute_intvlpwr(raw_data, t1, t2, avg = true, dim = 2)
-        nt = t2-t1
-        compute_pwr(raw_data[:, (t1:t2), :], nt, avg, dim)
-    end
+    # function compute_intvlpwr(raw_data, t1, t2, avg = true, dim = 2)
+    #     nt = t2-t1
+    #     compute_pwr(raw_data[:, (t1:t2), :], nt, avg, dim)
+    # end
 
     function compute_singlepwr(raw_data, idx)
-        return compute_mag(raw_data[:, idx, :])
+        return compute_mag(@view raw_data[:, idx, :])
     end
 
     function hashpipe_fft(raw_data::Array{Complex{Int8}, 3}, n, chan,
                           np=2, nt=512*1024, nc=64, integrated=true)
         # single coarse channel FFT
-        reshaped = reshape(raw_data, (np, n, nt÷n, nc))[:, :, :, chan]
+        # @allocated
+        reshaped = @view reshape(raw_data, (np, n, nt÷n, nc))[:, :, :, chan]
         transformed = fft(reshaped, 2)
         shifted = fftshift(transformed, 2) #(2, n, nt/n, nchans)
         if integrated
@@ -51,6 +52,6 @@ module HashpipeCalculations
 
     function remove_DCspike(pwr_array)
         n = size(pwr_array)[2]
-        pwr_array[:,n÷2 + 1] = (pwr_array[:,n÷2] + pwr_array[:,n÷2 + 2])/2
+        pwr_array[:,n÷2 + 1] = (@view pwr_array[:,n÷2] + pwr_array[:,n÷2 + 2])/2
     end
 end
