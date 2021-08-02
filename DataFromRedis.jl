c639f04225879225c07e5f29db06275dc269bcd3
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-02 19:53:23 +0000

    adding comments and clean up

--- DataFromRedis.jl
+++ DataFromRedis.jl
@@ -5,8 +5,9 @@ conn = RedisConnection(host="blh0")
 # srt://blc00/0/spectra
 
 function f(x)
-    println(x)
+    # grab data from redis
     abspwr = hget(conn, "srt://blc00/0/spectra", "avgpwr")
+    # convert abstract sring to power array
     pwrarray = reinterpret(Float32, codeunits(abspwr))
     avgpwr = reshape(pwrarray, (2,1,64))
 
@@ -14,16 +15,14 @@ function f(x)
     display(RedisDisplay.snapshot_power(avgpwr))
 end
 
-function exit(x)
-    
-    println("Connection Closed")
-end
-
+# open redis subscribtion
 sub = open_subscription(conn)
 
 try
+    # subsrcribe to channel where messages are being sent
     subscribe(sub, "chan-srt://blc00/0/spectra", f)
 catch e
+    # attempts are handling keyboard interruptions, doesnt work yet
     if isa(e, InterruptException)
         Redis.unsubscribe(sub, "chan-srt://blc00/0/spectra")
         println("Connection Closed.")
