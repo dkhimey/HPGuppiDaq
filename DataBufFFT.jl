82da60bfc5020bf2e0a3d9be64c7224fb0c10704
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-03 17:38:17 +0000

    improve design, integration with other modules

--- DataBufFFT.jl
+++ DataBufFFT.jl
@@ -2,50 +2,33 @@ include("HashpipeCalculations.jl")
 include("StatusBuff.jl")
 using .HashpipeCalculations, .StatusBuff, Hashpipe, FFTW, CircularArrayBuffers, Plots
 
-# databuffer properties
-inst, nbuff = params.inst, params.nbuff
-np, nt, nc = params.np, params.nt, params.nc
-nblocks = params.nblocks
+# attach to data buffer & get data in blocks
+datablocks = HashpipeCalculations.track_databuffer((0, 2, 24), (2, 512*1024, 64))
 
-ENV["HASHPIPE_KEYFILE"]="/home/davidm"
-# attach to data buffer
-pdb = Hashpipe.databuf_attach(inst, nbuff)
 # attach to status buffer
 st = Hashpipe.status_t(0,0,0,0)
 Hashpipe.status_attach(0, Ref(st))
-nblkin=String(fill(0x0, 80))
-
-datablocks = []
-nblock = 0
-while nblock < nblocks
-    println(nblock)
-    pb = Hashpipe.databuf_data(pdb, nblock)
-    pz = Ptr{Complex{Int8}}(pb+2560*80)
-    push!(datablocks, unsafe_wrap(Array, pz, (np, nt, nc)))
-    nblock+=1
-end
 
 # create circular array to store spectra
 # https://juliahub.com/ui/Packages/CircularArrayBuffers/HOj1r/0.1.2
 spectra = CircularArrayBuffer{Array{Float32, 2}}(5)
 
-# loop: read status buffer, calculate power of nblkin-1, add to circular array, display
-function read(n = 100, t = .1)
-    i = 0
-    while i < n
-        blk = StatusBuff.getnblkin(st, nblkin)
-        # println(blk) 
+# loop: read status buffer, FFT of nblkin-1, add to circular array, display
+function read(circarr = spectra , nf = 2^16, chan = 7, t = .1)
+    while true
+        blk = StatusBuff.getnblkin(st)
+        # *****improve this******
         if blk== 0
             blk = 24
         end
+        # ***********************
+
         # the nth block of the data buffer is at the n+1th index in datablocks
         # therefore, datablocks[n] is accessing the block before the one currently written
-        pwr = HashpipeCalculations.hashpipe_fft(datablocks[blk], 2^16, 7)
-        HashpipeCalculations.remove_DCspike(pwr)
-        push!(spectra, pwr)
-        display(plot(sum(spectra)[1, :], title = string(blk)))
-
-        i+=1
-        sleep(t)
+        pwr = HashpipeCalculations.hashpipe_fft(datablocks[blk], nf, chan) #compute fft
+        HashpipeCalculations.remove_DCspike(pwr) #remove DC spike
+        push!(circarr, pwr) #add to circular array
+        display(plot(sum(circarr)[1, :], title = string(blk))) #display integrated circular array
+        sleep(t) #wait
     end
 end
