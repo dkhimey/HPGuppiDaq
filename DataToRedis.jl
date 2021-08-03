840790775f66afbcf01b1b3c6f96bed8ed0727c2
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-03 18:19:30 +0000

    pushRedis function

--- DataToRedis.jl
+++ DataToRedis.jl
@@ -1,25 +1,26 @@
 include("HashpipeCalculations.jl")
 using .HashpipeCalculations, Redis
 
-inst, nbuff = 0, 2
-np, nt, nc = 2, 512*1024, 64
-nblock = 0
+# inst, nbuff = 0, 2
+# np, nt, nc = 2, 512*1024, 64
+# nblocks = 24
+# blks = HashpipeCalculations.track_databuffer((inst, nbuff, nblocks), (np, nt, nc))
 
-# track databuffer
-data = HashpipeCalculations.track_databuffer((inst, nbuff, nblock), (np, nt, nc))
-# connect to redis
-conn = RedisConnection(host="redishost")
-
-while true
-    # compute power
-    power = HashpipeCalculations.compute_pwr(data, nt)
-    # convert power array to abstract string
-    abspwr = unsafe_string(Ptr{UInt8}(pointer(power)), sizeof(power))
-    # add abstract string to redis
-    hset(conn, "srt://blc00/0/spectra", "avgpwr", abspwr)
-    # publish ready message
-    publish(conn, "chan-srt://blc00/0/spectra", "data ready")
-
-    # wait
-    sleep(2)
+function pushRedis(datablocks, t = 1)
+    # connect to redis
+    conn = RedisConnection(host="redishost")
+    while true
+        println("pushing.....")
+        # compute power
+        power = HashpipeCalculations.compute_pwr.(datablocks) #this takes forever
+        # convert power array to abstract string
+        abspwr = unsafe_string(Ptr{UInt8}(pointer(power)), sizeof(power))
+        # add abstract string to redis
+        hset(conn, "srt://blc00/0/spectra", "avgpwr", abspwr)
+        # publish ready message
+        publish(conn, "chan-srt://blc00/0/spectra", "data ready")
+    
+        # wait
+        sleep(t)
+    end
 end
