1777ed18e867dd249b1b1c381a3b2744aea34604
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-03 18:55:27 +0000

    correcting type errors, still needs improvement

--- StatusBuff.jl
+++ StatusBuff.jl
@@ -1,26 +1,27 @@
 module StatusBuff
     using Hashpipe, StringViews
+    # st = Hashpipe.status_t(0,0,0,0)
 
-    function track_statusbuff(inst)
-        st = Hashpipe.status_t(0,0,0,0)
+    function track_statusbuff(st, inst=0)
         Hashpipe.status_attach(inst, Ref(st))
         st_array = unsafe_wrap(Array, st.p_buf,
                     (Hashpipe.STATUS_RECORD_SIZE, Hashpipe.STATUS_TOTAL_SIZE÷Hashpipe.STATUS_RECORD_SIZE))
         return StringView.(eachcol(st_array))
     end
 
-    function getStatus(stsv, field, type=Int)
+    function getStatus(st, field, inst=0, type=Int)
+        stsv = track_statusbuff(st, inst)
         Hashpipe.status_buf_lock_unlock(Ref(st)) do
             return parse(type, @view stsv[findfirst(startswith(field), stsv)][10:end])
         end
     end
 
-    function getnblkin(stsv)
+    function getnblkin(st, inst=0)
         # attempt to access NULBLKIN field
-        try return getStatus(stsv, "NULBLKIN")
+        try return getStatus(st, "NULBLKIN", inst)
         catch
             # if NULBLKIN field does not exist, calculate using PKTIDX
-            return getStatus(stsv, "PKTIDX") ÷ 16384 % 24
+            return getStatus(st, "PKTIDX", inst) ÷ 16384 % 24
         end
     end
 
