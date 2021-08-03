80c10d70cbeb320cb30ab8f56b46252e980c2f62
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-03 18:23:07 +0000

    clean up

--- RedisDisplay.jl
+++ RedisDisplay.jl
@@ -1,8 +1,6 @@
 module RedisDisplay
 using Plots, Statistics
     function snapshot_power(avg_pwr)
-        # size(avg_pwr) == (2,1,64)
-
         l1 = @layout [a ; b]
         pol1 = avg_pwr[1,1,:]
         pol2 = avg_pwr[2,1,:]
@@ -13,23 +11,6 @@ using Plots, Statistics
         p = plot(p1, p2, layout = l1, legend = false, titlefontsize=10, 
                 xlabel="Channel", ylabel="avg power")
 
-        # l1 = @layout [a ; b]
-        # h1 = histogram(real.(data[1,:,1]))
-        # h2 = histogram(real.(data[2,:,1]))
-        # h = plot(h1, h2, layout = l1, legend = false, xlabel="Re(voltage)")
-
-        # l2 = @layout [a{0.7w} b]
-        # return plot(p, h, layout = l2)
         return p
-    end
-
-    function display_power(avg_pwr, t = .5)
-        i = 0
-        while true
-            println(i)
-            display(snapshot_power(avg_pwr))
-            i+=1
-            sleep(t)
-        end
     end
 end
