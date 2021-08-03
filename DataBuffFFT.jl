d0a36f5221d8d6bc226a620f1578a92927a3cfd4
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-03 18:41:43 +0000

    rename DataBuffFFT.jl

---
+++ DataBuffFFT.jl
@@ -0,0 +1,26 @@
+module DataBuffFFT
+include("HashpipeUtils.jl")
+include("StatusBuff.jl")
+using .HashpipeUtils, .StatusBuff, Hashpipe, FFTW, CircularArrayBuffers, Plots
+
+# loop: read status buffer, FFT of nblkin-1, add to circular array, display
+function FFTread(datablocks, circarr, chan, stsv, nf = 2^16, t = 1)
+    while true
+        blk = StatusBuff.getnblkin(stsv)
+        # *****improve this******
+        if blk== 0
+            blk = 24
+        end
+        # ***********************
+
+        # the nth block of the data buffer is at the n+1th index in datablocks
+        # therefore, datablocks[n] is accessing the block before the one currently written
+        pwr = HashpipeUtils.hashpipe_fft(datablocks[blk], nf, chan) #compute fft
+        HashpipeUtils.remove_DCspike(pwr) #remove DC spike
+        push!(circarr, pwr) #add to circular array
+        display(plot(sum(circarr)[1, :], title = string(blk))) #display integrated circular array
+        sleep(t) #wait
+    end
+end
+
+end
