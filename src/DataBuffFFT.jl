8d4c4e54921be9cc3cb0417c7dc2f3777142c696
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-06 23:07:05 +0000

    working on moving total pwr

--- src/DataBuffFFT.jl
+++ src/DataBuffFFT.jl
@@ -1,27 +1,34 @@
 module DataBuffFFT
-include("HashpipeUtils.jl")
-include("StatusBuff.jl")
-using .HashpipeUtils, .StatusBuff, Hashpipe, FFTW, CircularArrayBuffers, Plots
+    include("HashpipeUtils.jl")
+    include("StatusBuff.jl")
+    using Hashpipe, FFTW, CircularArrayBuffers, Plots
 
-# loop: read status buffer, FFT of nblkin-1, add to circular array, display
-function FFTread(datablocks, circarr, chan, st, inst = 0,
-                 nf = 2^16, nblocks = 24,t = 1)
-    while true
-        blk = StatusBuff.getnblkin(st, inst)
-        # *****improve this******
-        if blk== 0
-            blk = nblocks
+    # loop: read status buffer, FFT of nblkin-1, add to circular array, display
+    function FFTread(datablocks, circarr, chan, st, inst = 0,
+                    nf = 2^16, nblocks = 24,t = 1)
+        totalpower = zeros(size(datablocks[1]))
+        while true
+            blk = StatusBuff.getnblkin(st, inst)
+            # *****improve this******
+            if blk== 0
+                blk = nblocks
+            end
+            # ***********************
+
+            # the nth block of the data buffer is at the n+1th index in datablocks
+            # therefore, datablocks[n] is accessing the block before the one currently written
+            fft = HashpipeUtils.hashpipe_fft(datablocks[blk], nf, chan) 
+            pwr = HashpipeUtils.compute_pwr(fft, false, 3)[:,:, 1] #compute fft, pwr
+            # pwr = reshape(pwr, (size(fft)[1], nf))
+            HashpipeUtils.remove_DCspike(pwr) #remove DC spike
+            # store new spectrum in circarr
+            push!(circarr, pwr)
+
+            # update the total
+            # total = HashpipeUtils.integrate(circ)
+
+            # display(plot(sum(circarr)[1, :], title = string(blk))) #display integrated circular array
+            sleep(t) #wait
         end
-        # ***********************
-
-        # the nth block of the data buffer is at the n+1th index in datablocks
-        # therefore, datablocks[n] is accessing the block before the one currently written
-        pwr = HashpipeUtils.hashpipe_fft(datablocks[blk], nf, chan) #compute fft
-        HashpipeUtils.remove_DCspike(pwr) #remove DC spike
-        push!(circarr, pwr) #add to circular array
-        display(plot(sum(circarr)[1, :], title = string(blk))) #display integrated circular array
-        sleep(t) #wait
     end
-end
-
 end
