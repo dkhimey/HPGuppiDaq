85eb4d7b509034a47fc1f8b69f98a7629d1b1e7d
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-10 20:00:37 +0000

    rewrite FFTread function: calculate totalpower

--- src/DataBuffFFT.jl
+++ src/DataBuffFFT.jl
@@ -1,34 +1,37 @@
 module DataBuffFFT
     include("HashpipeUtils.jl")
     include("StatusBuff.jl")
-    using Hashpipe, FFTW, CircularArrayBuffers, Plots
+    using Hashpipe, FFTW, CircularArrayBuffers, Plots, ..HashpipeUtils
 
-    # loop: read status buffer, FFT of nblkin-1, add to circular array, display
-    function FFTread(datablocks, circarr, chan, st, inst = 0,
-                    nf = 2^16, nblocks = 24,t = 1)
-        totalpower = zeros(size(datablocks[1]))
+    # loop: read status buffer, FFT of nblkin-1, update total power, perform function on total power
+    function FFTread(datablocks, n, chan, func, st, inst = 0,
+        nf = 2^16, nblocks = 24,t = 1)
+        totalpower = zeros((size(datablocks[1],1), nf))
+        circarr = HashpipeUtils.init_circarray(n)
         while true
             blk = StatusBuff.getnblkin(st, inst)
             # *****improve this******
             if blk== 0
-                blk = nblocks
+            blk = nblocks
             end
             # ***********************
-
             # the nth block of the data buffer is at the n+1th index in datablocks
             # therefore, datablocks[n] is accessing the block before the one currently written
             fft = HashpipeUtils.hashpipe_fft(datablocks[blk], nf, chan) 
             pwr = HashpipeUtils.compute_pwr(fft, false, 3)[:,:, 1] #compute fft, pwr
-            # pwr = reshape(pwr, (size(fft)[1], nf))
             HashpipeUtils.remove_DCspike(pwr) #remove DC spike
+
+            # subtract oldest power
+            totalpower -= circarr[1]
+            # add newest power
+            totalpower += pwr
             # store new spectrum in circarr
             push!(circarr, pwr)
 
-            # update the total
-            # total = HashpipeUtils.integrate(circ)
-
+            func(totalpwr)
             # display(plot(sum(circarr)[1, :], title = string(blk))) #display integrated circular array
             sleep(t) #wait
         end
     end
 end
+
