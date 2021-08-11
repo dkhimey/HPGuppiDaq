cae8ddeff26db463653987f7d1f4f5ccc0679967
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-11 23:28:20 +0000

    accessing status buffer fields

--- tests/example1.jl
+++ tests/example1.jl
@@ -3,11 +3,14 @@ using HPGuppiDaq, Hashpipe
 # !!specify path to Hashpipe Keyfile!!
 ENV["HASHPIPE_KEYFILE"]="/home/davidm"
 
-# attach to data buffer & grab data in blocks
+# attach to data buffer
 inst, nbuff, nblocks = (0, 2, 24)
 np, nt, nc = (2, 512*1024, 64)
+# grab data in blocks
 blks = HPGuppiDaq.HashpipeUtils.track_databuffer((inst, nbuff, nblocks), (np, nt, nc))
 
 # attach to status buffer
 shmid, p_lock, p_buf = 0,0,0
 st = Hashpipe.status_t(inst, shmid, p_lock, p_buf)
+# grab fields in status buffer
+status = HPGuppiDaq.HashpipeUtils.track_statusbuff(st)
