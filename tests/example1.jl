d4579e3c1da50e45d0bfb28b37f464cfd9f1ef51
Diana Khimey <dianak@blc00.bl.pvt>
2021-08-11 23:13:29 +0000

    attaching to data and status buffers, loading in data

--- tests/example1.jl
+++ tests/example1.jl
@@ -1,17 +1,13 @@
-include("../HashpipeUtils.jl")
-include("../DataBuffFFT.jl")
-include("../StatusBuff.jl")
-using .HashpipeUtils, .DataBuffFFT, .StatusBuff, Hashpipe, CircularArrayBuffers
+using HPGuppiDaq, Hashpipe
 
-# ENV["HASHPIPE_KEYFILE"]="/home/davidm"
-# attach to data buffer & get data in blocks
-blks = HashpipeUtils.track_databuffer((0, 2, 24), (2, 512*1024, 64))
+# !!specify path to Hashpipe Keyfile!!
+ENV["HASHPIPE_KEYFILE"]="/home/davidm"
+
+# attach to data buffer & grab data in blocks
+inst, nbuff, nblocks = (0, 2, 24)
+np, nt, nc = (2, 512*1024, 64)
+blks = HPGuppiDaq.HashpipeUtils.track_databuffer((inst, nbuff, nblocks), (np, nt, nc))
 
 # attach to status buffer
-st = Hashpipe.status_t(0,0,0,0)
-
-# create circular array to store spectra
-# https://juliahub.com/ui/Packages/CircularArrayBuffers/HOj1r/0.1.2
-spectra = CircularArrayBuffer{Array{Float32, 2}}(5)
-
-# DataBuffFFT.FFTread(blks, spectra, 7, st)
+shmid, p_lock, p_buf = 0,0,0
+st = Hashpipe.status_t(inst, shmid, p_lock, p_buf)
